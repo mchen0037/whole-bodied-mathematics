8cceccc9ac005451e0b97c0ee233fb3fda23efb4
Mighty Chen <mchen73@ucmerced.edu>
2022-01-30 13:16:04 -0800

    rename folder

--- app/generate_marker_data.py
+++ app/generate_marker_data.py
@@ -10,7 +10,7 @@ from utils.constants import constants as C
 # Change this as we go through each camera
 camera_id = 1
 
-READ_PATH = "camera_world_calibration/callibration_transformation_data_images/camera_" + str(camera_id) + "/"
+READ_PATH = "camera_world_calibration/images/camera_" + str(camera_id) + "/"
 SAVE_PATH = "camera_world_calibration/camera_" + str(camera_id) + "_data.csv"
 print(READ_PATH)
 
