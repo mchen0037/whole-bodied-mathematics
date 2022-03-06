54f1bc84e7fb5b4086d5cdeccaa901a6040c4fd5
Mighty Chen <mchen73@ucmerced.edu>
2022-03-06 12:50:46 -0800

    remove save video because it makes system super laggy

--- app/utils/MocapSystem.py
+++ app/utils/MocapSystem.py
@@ -152,10 +152,11 @@ class MocapSystem(object):
 
                 # FIXME: Hack-y way to handle this for now. Computer can't keep up
                 # with saving all 4 video streams.
-                if int(cam) == 3:
-                    camera_meta["save_video"] = self.save_video
-                else:
-                    camera_meta["save_video"] = False
+                # if int(cam) == 3:
+                    # camera_meta["save_video"] = self.save_video
+                # else:
+                # Never save video for now.
+                camera_meta["save_video"] = False
                 camera_id_meta_dict[int(cam)] = camera_meta
 
         # After finding all camera matrices, make sure we assert that we have
